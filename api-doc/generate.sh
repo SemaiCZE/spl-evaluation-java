bfc1c80f4d065d9890ef322a4afdfa349a126901
Petr Stefan <ptr.stef@gmail.com>
2017-11-22 13:36:05 +0100

    Docs

--- api-doc/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf data_server/
-
-java -jar swagger-codegen-cli-2.2.2.jar generate -i swagger.yaml -l msf4j -o data_server --api-package cz.cuni.mff.d3s.spl.restapi -DhideGenerationTimestamp=true
-
